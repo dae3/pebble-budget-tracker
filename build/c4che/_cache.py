b6246f2cac4175dfff907a8d06b73fd014d92ad0
Daniel Everett <deverett@gmail.com>
2016-04-03 09:16:58 +1000

    Excluded build folder

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/home/deverett/.pebble-sdk/SDKs/current/sdk-core/pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'basalt']
